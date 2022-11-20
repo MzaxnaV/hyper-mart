23b84208896fded483cd827b2bf92b95bdc528f3
Manav <30342951+MzaxnaV@users.noreply.github.com>
2022-11-20 00:16:39 +0000

    database done

--- hyperMart/dashboard/models.py
+++ hyperMart/dashboard/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.files import File
 
 # Create your models here.
+
+
+# class ProductImage(models.Model):
+#     image_file = models.ImageField(upload_to='images')
+#     image_url = models.URLField()
+
+#     def save(self, *args, **kwargs):
+#         if self.image_url:
+#             r = requests.get(self.image_url)
+#             self.image_file.save(
+#                 os.path.basename(self.image_url),
+#                 io.BytesIO(r.content)
+#             )
+#             super(ProductImage, self).save(args, kwargs)
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200)
+    retail_price = models.IntegerField(default=0)
+    discounted_price = models.IntegerField(default=0)
+    brand = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    image_url = models.URLField()
--- hyperMart/hyperMart/settings.py
+++ hyperMart/hyperMart/settings.py
@@ -27,10 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MEDIA_ROOT = "/workspaces/hyper-mart/hyperMart/images/"
+
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [#
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
